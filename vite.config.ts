77c28f74862f0987090f1976bff8571554127f27
Aleko Khomasuridze <207458288+SAA-2025@users.noreply.github.com>
2025-04-14 13:10:10 +0400

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/SAA_View', 
+  base: '/SAA_View/', 
   plugins: [react()],
 })
 
